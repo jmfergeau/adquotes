223c018dda01c10703473247869ecdaafe868d87
Max le Fou <buttbadger@gmail.com>
2019-07-15 16:24:16 +0000

    Téléverser un nouveau fichier

---
+++ options.js
@@ -0,0 +1,11 @@
+// Put here the number of miliseconds you want for the quotes to change. By defautl, it's 10 seconds, so it's 10000.
+var secs = 10000;
+
+// Put here the style of desired borders for the quotes box.
+// Choices are: "none", "solid", "double", "random".
+var borderStyle = "random";
+
+// Put here the desired color of borders for the quotes box.
+// Can be any HTML color code like "#FF0000" or "red".
+// Pur "random" for random colors.
+var borderColor = "random";
