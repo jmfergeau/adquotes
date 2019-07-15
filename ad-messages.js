ab29dddd6033417ae70a758d0fb08d170bf04cee
Max le Fou <buttbadger@gmail.com>
2019-07-15 16:24:00 +0000

    Replace ad-messages.js

--- ad-messages.js
+++ ad-messages.js
@@ -1,39 +1,3 @@
-// START EDITING
-// ================================================================================================
-// Put here the number of miliseconds you want for the quotes to change. By defautl, it's 10 seconds, so it's 10000.
-var secs = 10000;
-
-// Put here the style of desired borders for the quotes box.
-// Choices are: "none", "solid", "double", "random".
-var borderStyle = "random";
-
-// Put here the desired color of borders for the quotes box.
-// Can be any HTML color code like "#FF0000" or "red".
-// Pur "random" for random colors.
-var borderColor = "random";
-
-var strings = [
-// These lines are the quotes the script will randomly choose. You can put as much as you want.
-// If you absolutely need to put " inside your quotes, use either ' or \".
-  // Don't forget, the quotes have a , after the "" !!
-  "Ceci n'est pas une pipe. ~~Magritte",
-  "Keep love in your heart. A life without it is like a sunless garden when the flowers are dead. ~~Oscar Wilde",
-  "The most difficult thing is the decision to act, the rest is merely tenacity. The fears are paper tigers. You can do anything you decide to do. You can act to change and control your life; and the procedure, the process is its own reward. ~~Amelia Earhart",
-  "Love isn't something you find. Love is something that finds you. ~~Loretta Young",
-  "Good judgment comes from experience, and a lot of that comes from bad judgment. ~~Will Rogers",
-  "Honesty is the first chapter in the book of wisdom. ~~Thomas Jefferson",
-  "Today you are you! That is truer than true! There is no one alive who is you-er than you! ~~Dr. Seuss",
-  "Success is not final, failure is not fatal: it is the courage to continue that counts. ~~Winston Churchill",
-  "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. ~~Helen Keller",
-  "It is during our darkest moments that we must focus to see the light. ~~Aristotle",
-  "Strange women lying in ponds, distributing swords, is no basis for a system of government! ~~Monty Python's Holy Grail",
-  "Are you suggesting that coconuts migrate? ~~Monty Python's Holy Grail"
-  // Don't forget, the last quote DOES NOT have a , after the "" !!
-];
-// ================================================================================================
-// END EDITING
-// Don't forget, the last quote DOES NOT have a , after the "" !!
-
 window.setInterval(function() {
   // puts a preselection of styles for the border Originally, there's only those 3 ones.
   var borderStylesArray = ["none", "solid", "double"];
